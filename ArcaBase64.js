4ad875a1bf272060fa45f0b09cf047490e0bc0fb
Ko Hyeongseok <kohs102@gmail.com>
2021-06-03 12:13:05 +0900

    Update ArcaBase64.js
    1. 디코딩 깊이 3까지 자동처리
    2. 선택후 클릭으로 디코딩 기능

--- ArcaBase64.js
+++ ArcaBase64.js
@@ -6,31 +6,78 @@
 // @run-at       document-end
 // ==/UserScript==
 
-const reghttp = /(aHR0cDovL|aHR0cHM6Ly)(\w|=)*(?=[^=\w])/g;
-var hindex = 0;
+const regArr = [
+    /(aHR0cDovL|aHR0cHM6Ly)(\w|=)*(?=[^=\w])/g,
+    /(YUhSMGNEb3ZM|YUhSMGNITTZMe)(\w|=)*(?=[^=\w])/g,
+    /(WVVoU01HTkViM1pN|WVVoU01HTklUVFpNZ)(\w|=)*(?=[^=\w])/g,
+]
+const regInvalid = /[^A-Za-z0-9+\/=]/;
 
-function createLink(index, url) {
-    return '<a href="' + url + '" target="_blank">' + index.toString() + '번째 링크 (base64에서 변환됨)</a>'
+var hindex = 0;
+var lastSelected = document;
+var lastSelectedTime = Date.now();
+
+function createLink(index, url, depth) {
+    return '<a href="' + url
+           + '" target="_blank">' + index.toString()
+           + '번째 링크 (base64 깊이: ' + depth.toString()
+           + ')</a>'
 }
 
-function replaceCode(match) {
-    try {
-        var converted = atob(match);
-        hindex++;
-        return createLink(hindex, converted);
-    } catch(e) {
-        console.log(e);
-        console.log('base64 변환 실패:' + match);
+function replacerGen(numIter) {
+    return function(match) {
+        try {
+            var converted = atob(match);
+            for(var i=0; i<numIter; i++) {
+                converted = atob(converted);
+            }
+            hindex++;
+            return createLink(hindex, converted, numIter+1);
+        } catch(e) {
+            console.log(e);
+            console.log('base64 변환 실패:' + match);
+        }
+        return match + 'base64 변환 실패.';
     }
-    return match + 'base64 변환 실패.';
+}
+
+function selClicked(event) {
+    var sel = document.getSelection().toString();
+    if (!sel.match(regInvalid)
+        && sel.length >= 10
+        && lastSelectedTime + 100 < Date.now()) {
+        try {
+            var converted = atob(sel);
+        } catch (e) {
+            return;
+        } finally {
+            this.innerHTML = this.innerHTML.replace(sel, converted);
+            this.removeEventListener('click', selClicked);
+        }
+    }
 }
 
 (function() {
     'use strict';
 
     var article = document.getElementsByClassName("article-content")[0];
-    var comments = document.getElementsByClassName("article-comment")[0];
+    var comments = document.getElementsByClassName("list-area")[0];
 
-    article.innerHTML = article.innerHTML.replaceAll(reghttp, replaceCode);
-    comments.innerHTML = comments.innerHTML.replaceAll(reghttp, replaceCode);
+    for(var i=0; i<3; i++) {
+        article.innerHTML = article.innerHTML.replaceAll(regArr[i], replacerGen(i));
+        comments.innerHTML = comments.innerHTML.replaceAll(regArr[i], replacerGen(i));
+    }
+
+    document.addEventListener('selectionchange', function() {
+        var sel = document.getSelection().anchorNode;
+        if(sel) {
+            sel = sel.parentElement;
+            if(sel != lastSelected) {
+                lastSelected.removeEventListener('click', selClicked);
+                sel.addEventListener('click', selClicked);
+                lastSelected = sel;
+                lastSelectedTime = Date.now();
+            }
+        }
+    })
 })();
